efab4d5b23743e84146d6c817243f3df9417ecc2
Leo Hardt <leom.hardt@gmail.com>
2024-08-04 18:27:20 -0300

    Finished T3?

--- T3/solucao.py
+++ T3/solucao.py
@@ -1,4 +1,6 @@
 from typing import Iterable, Set, Tuple
+from queue import PriorityQueue
+from queue import Queue
 
 class Nodo:
     """
@@ -16,15 +18,24 @@ class Nodo:
         self.pai = pai
         self.acao = acao
         self.custo = custo
+        self.heuristic = 0
         # substitua a linha abaixo pelo seu codigo
         #raise NotImplementedError
 
     def __eq__(self, other):
+        if other is None:
+            return self is None 
         return self.estado == other.estado  and self.custo == other.custo
         
-    def __hash__(self,other):
-        return estado + "#" + acao + "#" + custo
-        
+    def __hash__(self):
+        return (self.estado + "#" + str(self.acao) + "#" + str(self.custo)).__hash__()
+
+    def __lt__(self, other):
+        return (self.getTotalHeuristic()) < (other.getTotalHeuristic()) 
+
+    def getTotalHeuristic(self):
+        return self.custo + self.heuristic
+            
 UP   = "acima"
 DOWN = "abaixo"
 LEFT = "esquerda"
@@ -54,6 +65,7 @@ def getNeighbours(stringPos):
     if stringPos == 8:
         return [(UP,   5),  (LEFT, 7)                        ]
     
+    print(" Neighbour for <" + str(stringPos) + ">?")
     raise NotImplementedError
 
 def stringSwap(estado:str, i1:int, i2:int):
@@ -71,13 +83,14 @@ def sucessor(estado:str)->Set[Tuple[str,str]]:
     :return:
     """
     # substituir a linha abaixo pelo seu codigo
-    blankPostion = estado.find("_")
+    blankPostion = estado.find('_')
     neighbours = getNeighbours(blankPostion)
     succ = set()
     for neigh in neighbours:
         succ.add((neigh[0], stringSwap(estado, blankPostion, neigh[1])))
     return succ
     
+
 def expande(nodo:Nodo)->Set[Nodo]:
     """
     Recebe um nodo (objeto da classe Nodo) e retorna um conjunto de nodos.
@@ -88,7 +101,7 @@ def expande(nodo:Nodo)->Set[Nodo]:
     novosEstados = sucessor(nodo.estado)
     novosNodos = set()
     for novoEstado in novosEstados: 
-        novoNodo = Nodo(novoEstado[0], self, novoEstado[0], self.custo+1)
+        novoNodo = Nodo(novoEstado[1], nodo, novoEstado[0], nodo.custo+1)
         novosNodos.add(novoNodo)
     return novosNodos
 
@@ -101,8 +114,57 @@ def make_hamming_dist(e1:str, e2:str):
    
 def make_manhattan_dist(e1:str, e2:str):
     total = 0
-    # TODO
+    for i in range(len(e1)):
+        if e1[i] != e2[i] and e1[i] != '_':
+            posicao_atual = divmod(i, 3)
+            alvo = divmod(int(e1[i]) - 1, 3)
+            total += abs(posicao_atual[0] - alvo[0]) + abs(posicao_atual[1] - alvo[1])
     return total
+        
+def astar(estado:str, heuristicFunc)->list[str]:
+    """
+    Recebe um estado (string), executa a busca A* com h(n) = soma das distâncias de Hamming e
+    retorna uma lista de ações que leva do
+    estado recebido até o objetivo ("12345678_").
+    Caso não haja solução a partir do estado recebido, retorna None
+    :param estado: str
+    :return:
+    """
+    FINAL = "12345678_"
+    explored = set()
+    frontier = PriorityQueue()
+    frontier.put(( 
+        heuristicFunc(estado, FINAL) ,   Nodo(estado, None, None, heuristicFunc(estado, FINAL) ) 
+    ))
+    
+    finalNode = None
+    while not frontier.empty() :
+        item = frontier.get()
+        currNode = item[1]
+        
+        if currNode in explored:
+            continue
+        if currNode.estado == FINAL:
+            finalNode = currNode
+            break
+
+        explored.add(currNode.estado)
+        
+        neighbours = expande(currNode)
+        for neigh in neighbours:
+            neigh.heuristic =  heuristicFunc(estado, FINAL)
+            
+            if neigh.estado not in explored:     
+                frontier.put((neigh.getTotalHeuristic(), neigh))
+        
+    actions = []
+    while finalNode != None and finalNode.acao != None:
+        actions = [finalNode.acao] + actions
+        finalNode = finalNode.pai
+          
+    if len(actions) == 0:
+        actions = None
+    return actions
 
 def astar_hamming(estado:str)->list[str]:
     """
@@ -113,8 +175,7 @@ def astar_hamming(estado:str)->list[str]:
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    return astar(estado, make_hamming_dist)
 
 
 def astar_manhattan(estado:str)->list[str]:
@@ -126,8 +187,7 @@ def astar_manhattan(estado:str)->list[str]:
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    return astar(estado, make_manhattan_dist)  
 
 #opcional,extra
 def bfs(estado:str)->list[str]:
@@ -139,10 +199,60 @@ def bfs(estado:str)->list[str]:
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    # Ou, simplesmente 
+    #astar(estado,  lambda x:  "ordemDeChegada" ) 
+    FINAL = "12345678_"
+    explored = set()
+    frontier = Queue()
+    frontier.put((0,   Nodo(estado, None, None, 0)))
+    
+    finalNode = None
+    while not frontier.empty() :
+        item = frontier.get()
+        currNode = item[1]
+        
+        if currNode in explored:
+            continue
+        if currNode.estado == FINAL:
+            finalNode = currNode
+            break
+
+        explored.add(currNode.estado)
+        
+        neighbours = expande(currNode)
+        for neigh in neighbours:
+            
+            if neigh.estado not in explored:     
+                frontier.put((neigh.custo, neigh))
+        
+    actions = []
+    while finalNode != None and finalNode.acao != None:
+        actions = [finalNode.acao] + actions
+        finalNode = finalNode.pai
+          
+    if len(actions) == 0:
+        actions = None
+    return actions
 
 #opcional,extra
+def dfs_aux(nodo, explorados):
+    FINAL = "12345678_"
+
+    if nodo.estado == FINAL:
+        return [nodo.acao]
+
+    neighbours = expande(nodo)
+    
+    explorados = explorados + [nodo.estado]
+    for neigh in neighbours:
+        if neigh.estado in explorados:
+            continue
+            
+        ans = dfs_aux(neigh, explorados)
+        if ans is not None: 
+            return [nodo.acao] + ans
+    return None
+
 def dfs(estado:str)->list[str]:
     """
     Recebe um estado (string), executa a busca em PROFUNDIDADE e
@@ -152,10 +262,9 @@ def dfs(estado:str)->list[str]:
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
-
-#opcional,extra
+    atual = Nodo(estado, None, None, 0)
+    return dfs_aux(atual, [])
+        
 def astar_new_heuristic(estado:str)->list[str]:
     """
     Recebe um estado (string), executa a busca A* com h(n) = sua nova heurística e
@@ -165,5 +274,6 @@ def astar_new_heuristic(estado:str)->list[str]:
     :param estado: str
     :return:
     """
-    # substituir a linha abaixo pelo seu codigo
-    raise NotImplementedError
+    # Comparing the position of the slot with the final position. Closer is better.
+    return astar(estado, (lambda e1, e2 : 10 - e1.find("_"))) 
+
